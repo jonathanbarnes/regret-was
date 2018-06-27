2e1b4bd8de74f17be8d46d5860e6a92ce6999db1
Jonathan <jonathan.barnes@asos.com>
2018-06-27 21:58:04 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true }
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader' // creates style nodes from JS strings
+					},
+					{
+						loader: 'css-loader' // translates CSS into CommonJS
+					},
+					{
+						loader: 'sass-loader' // compiles Sass to CSS
+					}
+				]
+			},
+			{
+				// Match woff2 in addition to patterns like .woff?v=1.1.1.
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						// Limit at 50k. Above that it emits separate files
+						limit: 50000,
+
+						// url-loader sets mimetype if it's passed.
+						// Without this it derives it from the file extension
+						mimetype: 'application/font-woff',
+
+						// Output below fonts directory
+						name: './fonts/[name].[ext]'
+					}
+				}
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: ['url-loader?limit=10000', 'img-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		})
+	]
+};
