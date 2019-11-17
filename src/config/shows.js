79b21032d29d626e8d73bcb9e00d8e63973b3568
Jonathan <jonathan.barnes@live.co.uk>
2019-11-17 21:04:40 +0000

    remove cock and balls

--- src/config/shows.js
+++ src/config/shows.js
@@ -1,8 +1,8 @@
 export default [
-	{
-		date: "10.11",
-		venue: "The Cock and Balls",
-		city: "Chelmsford",
-		link: "http://instagram.com"
-	}
+	// {
+	// 	date: "10.11",
+	// 	venue: "The Cock and Balls",
+	// 	city: "Chelmsford",
+	// 	link: "http://instagram.com"
+	// }
 ];
